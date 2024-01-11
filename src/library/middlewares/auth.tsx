691a7e6da839e600a166cf6b83c6a01b1d4b5efa
hart harney <hartharney@gmail.com>
2024-01-11 18:14:28 +0100

    modify middleware and attach request interface

--- src/library/middlewares/auth.tsx
+++ src/library/middlewares/auth.tsx
@@ -2,55 +2,23 @@ import { NextFunction, Request, Response } from "express";
 import Jwt, { JwtPayload } from "jsonwebtoken";
 import { UsersModel } from "../../components/users/model";
 
-//================== I changed from this class based middleware, because I dont see the need in this project, 
-//since it could be simpler ==================//
- 
-// export class AuthMiddleware {
-//   static Authenticate =
-//     (auth: string) =>
-//     async (req: Request, res: Response, next: NextFunction) => {
-//       try {
-//         const token = req.headers["Authorization"] as string;
-//         console.log(token);
- 
-//         if (!token) {
-//           return res
-//             .status(401)
-//             .json({ error: "unauthorized, no token provided" });
-//         }
-//         const verified = Jwt.verify(token, process.env.JWT_SECRET!);
-//         if (!verified) {
-//           return res.status(401).json({ error: "unauthorized" });
-//         }
-//         const { id } = verified as JwtPayload;
-//         const user = (await UsersModel.findOne({
-//           where: { id },
-//         })) 
-//         if (!user) {
-//           return res.status(401).json({ error: "unauthorized" });
-//         }
-//         req.user = user.dataValues;
-//         next();
-//       } catch (error) {
-//         console.error("Authentication error:", error);
-//         return res.status(500).json({ error });
-//       }
-//     };
-// }
- 
-
 const authenticateMiddleware = async (req: Request, res: Response , next: NextFunction) => {
   try {
-    const token = req.cookies.token;
+    let token;
+    const auth = req.headers.authorization;
+
+    if(auth){
+     token = auth
+    }
 
     if (!token) {
-      return res.status(401).json({ error: 'unauthorized, no token provided' });
+      return res.status(401).json({ error: 'Unauthorized, no token provided' });
     }
 
     const verified = Jwt.verify(token, process.env.JWT_SECRET!);
 
     if (!verified) {
-      return res.status(401).json({ error: 'unauthorized' });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const { id } = verified as JwtPayload;
@@ -59,10 +27,10 @@ const authenticateMiddleware = async (req: Request, res: Response , next: NextFu
     });
 
     if (!user) {
-      return res.status(401).json({ error: 'unauthorized' });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    req.user = user.dataValues;
+    req.user = user.dataValues
     next();
   } catch (error) {
     console.error('Authentication error:', error);
