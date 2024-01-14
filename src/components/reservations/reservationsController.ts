ceb14aab3c1bb9b5ae4980252ab58a6552f9de39
hart harney <hartharney@gmail.com>
2024-01-14 13:35:28 +0100

    fix request req object in resrevations

--- src/components/reservations/reservationsController.ts
+++ src/components/reservations/reservationsController.ts
@@ -7,10 +7,10 @@ import {
   option,
 } from "../../utils/utils";
 import Jwt, { JwtPayload } from "jsonwebtoken";
-import { UnitsModel } from '../units/model'; // Import UnitsModel assuming it is correctly defined
+import { UnitsModel } from "../units/model"; // Import UnitsModel assuming it is correctly defined
+import UserRequest from "../../types/userRequest";
 
-
-export const createReservation = async (req: Request, res: Response) => {
+export const createReservation = async (req: UserRequest, res: Response) => {
   try {
     const {
       customerName,
@@ -35,15 +35,12 @@ export const createReservation = async (req: Request, res: Response) => {
 
     // Check if the unit with provided unitId exists
     const unit = await UnitsModel.findByPk(unitId);
-    
+
     if (!unit) {
       return res.status(400).json({ error: "Invalid unitId, unit not found" });
     }
 
-    const token = req.cookies.token;
-    const verified = Jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
-    const userId = verified.id;
-
+    const userId = req.user?.id;
     console.log("this is the userId: ", userId);
 
     const newReservation = await ReservationsModel.create({
@@ -68,128 +65,130 @@ export const createReservation = async (req: Request, res: Response) => {
   }
 };
 
+export const updateReservation = async (req: UserRequest, res: Response) => {
+  try {
+    const { id } = req.params;
+    const {
+      customerName,
+      customerEmail,
+      customerPhone,
+      checkInDate,
+      checkOutDate,
+      status,
+    } = req.body;
 
+    // Validate request body
+    const validate = updateReservationSchema.validate(req.body, option);
 
-export const updateReservation = async (req: Request, res: Response) => {
-    try {
-      const { id } = req.params;
-      const {
-        customerName,
-        customerEmail,
-        customerPhone,
-        checkInDate,
-        checkOutDate,
-        status,
-      } = req.body;
-  
-      // Validate request body
-      const validate = updateReservationSchema.validate(req.body, option);
-  
-      if (validate.error) {
-        return res.status(400).json({ error: validate.error.details[0].message });
-      }
-  
-      // Get user ID from the token
-      const token = req.cookies.token;
-      const verified = Jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
-      const userId = verified.id;
-  
-      // Check if the reservation exists and belongs to the user
-      const reservation = await ReservationsModel.findOne({ where: { id, userId } });
-  
-      if (!reservation) {
-        return res.status(404).json({ message: "Reservation not found" });
-      }
-  
-      // Update the reservation
-      const [affectedRows, updatedReservation] = await ReservationsModel.update(
-        { ...validate.value, userId },
-        { where: { id }, returning: true }
-      );
-  
-      // Check if the update was successful
-      if (affectedRows === 0) {
-        return res.status(500).json({ message: "Failed to update reservation" });
-      }
-  
-      return res.status(200).json({
-        updatedReservation: updatedReservation[0],
-        message: "Reservation updated successfully",
+    if (validate.error) {
+      return res.status(400).json({ error: validate.error.details[0].message });
+    }
+
+    // Get user ID from the token
+    const userId = req.user?.id;
+
+    // Check if the reservation exists and belongs to the user
+    const reservation = await ReservationsModel.findOne({
+      where: { id, userId },
+    });
+
+    if (!reservation) {
+      return res.status(404).json({ message: "Reservation not found" });
+    }
+
+    // Update the reservation
+    const [affectedRows, updatedReservation] = await ReservationsModel.update(
+      { ...validate.value, userId },
+      { where: { id }, returning: true }
+    );
+
+    // Check if the update was successful
+    if (affectedRows === 0) {
+      return res.status(500).json({ message: "Failed to update reservation" });
+    }
+
+    return res.status(200).json({
+      updatedReservation: updatedReservation[0],
+      message: "Reservation updated successfully",
+    });
+  } catch (error) {
+    console.error("Error during reservation update:", error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const reservationBelongingToUser = async (
+  req: UserRequest,
+  res: Response
+) => {
+  try {
+    // const { userId } = req.params; --- if the frontend person chooses to optionally use this approach
+
+    const userId = req.user?.id;
+
+    const reservation = await ReservationsModel.findAll({ where: { userId } });
+
+    if (!reservation) {
+      return res.status(404).json({ message: "reservation not found" });
+    }
+
+    return res.status(200).json({ reservation });
+  } catch (error) {
+    console.error("Error fetching reservation", error);
+    return res.status(500).json({ message: "something went wrong" });
+  }
+};
+
+export const reservationBelongingToUnit = async (
+  req: UserRequest,
+  res: Response
+) => {
+  try {
+    const { unitId } = req.params; // Assuming the unitId is in the request parameters
+
+    // If unitId is not in req.params, you can try to get it from req.body
+    // const { unitId } = req.body;
+
+    console.log(unitId);
+
+    if (!unitId) {
+      return res.status(400).json({
+        message: "unitId is required in the request parameters or body",
       });
-    } catch (error) {
-      console.error("Error during reservation update:", error);
-      return res.status(500).json({ message: "Something went wrong" });
     }
-  };
-  
 
-  export const reservationBelongingToUser = async (req: Request, res: Response) => {
-    try {
-      // const { userId } = req.params; --- if the frontend person chooses to optionally use this approach
-  
-      const token = req.cookies.token;
-      const verified = Jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
-      const userId = verified.id;
-  
-      const reservation = await ReservationsModel.findAll({ where: { userId } });
-  
-      if (!reservation) {
-        return res.status(404).json({ message: "reservation not found" });
-      }
-  
-      return res.status(200).json({ reservation });
-    } catch (error) {
-      console.error("Error fetching reservation", error);
-      return res.status(500).json({ message: "something went wrong" });
-    }
-  };
-  
+    const reservations = await ReservationsModel.findAll({ where: { unitId } });
 
-  export const reservationBelongingToUnit = async (req: Request, res: Response) => {
-    try {
-      const { unitId } = req.params; // Assuming the unitId is in the request parameters
-  
-      // If unitId is not in req.params, you can try to get it from req.body
-      // const { unitId } = req.body;
-  
-      console.log(unitId);
-  
-      if (!unitId) {
-        return res.status(400).json({ message: "unitId is required in the request parameters or body" });
-      }
-  
-      const reservations = await ReservationsModel.findAll({ where: { unitId } });
-  
-      if (!reservations || reservations.length === 0) {
-        return res.status(404).json({ message: "Reservations not found for the unit" });
-      }
-  
-      return res.status(200).json({ reservations });
-    } catch (error) {
-      console.error("Error fetching reservations", error);
-      return res.status(500).json({ message: "Something went wrong" });
+    if (!reservations || reservations.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Reservations not found for the unit" });
     }
-  };
-  
-  
 
-  export const getSingleReservation = async (req: Request, res: Response) => {
-    try {
-      const { id } = req.params;
-  
-      const token = req.cookies.token;
-      const verified = Jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
-      const userId = verified.id;
-  
-      const reservation = await ReservationsModel.findOne({ where: { id, userId } });
-  
-      if (!reservation) {
-        return res.status(404).json({ message: "reservation not found" });
-      }
-  
-      return res.status(200).json({ reservation });
-    } catch (error) {
-      console.error("Error during reservation update:", error);
-      return res.status(500).json({ message: "something went wrong" });
+    return res.status(200).json({ reservations });
+  } catch (error) {
+    console.error("Error fetching reservations", error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const getSingleReservation = async (req: UserRequest, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const userId = req.user?.id;
+
+    const reservation = await ReservationsModel.findOne({
+      where: { id, userId },
+    });
+
+    if (!reservation) {
+      return res.status(404).json({ message: "reservation not found" });
     }
-  };
+
+    return res.status(200).json({ reservation });
+  } catch (error) {
+    console.error("Error during reservation update:", error);
+    return res.status(500).json({ message: "something went wrong" });
+  }
+};
