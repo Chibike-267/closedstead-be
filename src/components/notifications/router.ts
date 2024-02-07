ca806454bc95473caceced9d6b66a0cded2e3afd
Kasie <anizobaadaobi@gmail.com>
2024-02-07 14:11:45 +0100

    notification model and notificationSchedule pages added

---
+++ src/components/notifications/router.ts
@@ -0,0 +1,7 @@
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+router.get("/notifications", (req: Request, res: Response) => {
+  res.status(200).json({ message: "success" });
+});
